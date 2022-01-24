9682679f0422809849eb7d9119b7f7dfac9b423e
tilakchad111 <tilakchad111@gmail.com>
2022-01-24 16:02:29 +0545

    Here comes the SIMD

--- src/maths/matrix.hpp
+++ src/maths/matrix.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+// Yay .. going to use SIMD
+#include <xmmintrin.h>
+
 #include "./vec.hpp"
 
 template <cNumeric T> struct Mat4
 {
-    // Might use row major (row first filling) semantics, Not goint with openGL style
+    // Might use row major (row first filling) semantics, Not going with openGL style
     T mat[4][4]      = {};
 
     constexpr Mat4() = default;
@@ -105,7 +108,7 @@ template <cNumeric T> struct Mat4
         return *this * matrix;
     }
 
-    constexpr Mat4 scale(Vec3<T>const& vec)
+    constexpr Mat4 scale(Vec3<T> const &vec)
     {
         Mat4 matrix(1);
         matrix[0][0] = vec.x;
@@ -153,14 +156,14 @@ inline Mat4f Perspective(float aspect_ratio, float fovy = 1.0f / 3 * 3.141592, f
     matrix[0][0]   = near_plane / right;
     matrix[1][1]   = near_plane / top;
     matrix[3][2]   = -1;
-    matrix[2][3]   = (- 2 * far_plane * near_plane) / (far_plane - near_plane);
+    matrix[2][3]   = (-2 * far_plane * near_plane) / (far_plane - near_plane);
     matrix[2][2]   = -(far_plane + near_plane) / (far_plane - near_plane);
     matrix[3][3]   = 0;
     return matrix;
 }
 
 inline Mat4f PerspectiveAlt(float aspect_ratio, float fovy = 1.0f / 3 * 3.141592, float near_plane = 0.1f,
-                         float far_plane = 10.0f)
+                            float far_plane = 10.0f)
 {
     Mat4f matrix(1.0f);
     float tan_fovy = std::sin(fovy) / std::cos(fovy);
@@ -170,17 +173,16 @@ inline Mat4f PerspectiveAlt(float aspect_ratio, float fovy = 1.0f / 3 * 3.141592
     matrix[0][0]   = near_plane / right;
     matrix[1][1]   = near_plane / top;
     matrix[3][2]   = -1;
-    matrix[2][3]   = (-1* far_plane * near_plane) / (far_plane - near_plane);
+    matrix[2][3]   = (-1 * far_plane * near_plane) / (far_plane - near_plane);
     matrix[2][2]   = -far_plane / (far_plane - near_plane);
     matrix[3][3]   = 0;
     return matrix;
 }
 
-template <cNumeric T> 
-std::ostream &operator<<(std::ostream& os, Mat4<T> const& matrix)
+template <cNumeric T> std::ostream &operator<<(std::ostream &os, Mat4<T> const &matrix)
 {
     os << "\nMatrix -> \n";
-    for (int i = 0;i < 4; ++i)
+    for (int i = 0; i < 4; ++i)
     {
         for (int j = 0; j < 4; ++j)
         {
@@ -190,3 +192,341 @@ std::ostream &operator<<(std::ostream& os, Mat4<T> const& matrix)
     }
     return os;
 }
+
+// Determinant of matrices
+// Should I write a linear algebra math functions here??
+
+inline Mat4f lookAtMatrix(const Vec3f &cameraPos, const Vec3f &target, const Vec3f &up)
+{
+    auto forward_vec = (cameraPos - target).unit();
+    auto right       = Vec3f::Cross(up, forward_vec).unit();
+
+    auto now_up      = Vec3f::Cross(forward_vec, right).unit();
+
+    Mat4 matrix(1.0f);
+    matrix[0][0] = right.x;
+    matrix[0][1] = right.y;
+    matrix[0][2] = right.z;
+    matrix[0][3] = -cameraPos.dot(right);
+
+    matrix[1][0] = now_up.x;
+    matrix[1][1] = now_up.y;
+    matrix[1][2] = now_up.z;
+
+    matrix[1][3] = -cameraPos.dot(now_up);
+
+    matrix[2][0] = forward_vec.x;
+    matrix[2][1] = forward_vec.y;
+    matrix[2][2] = forward_vec.z;
+    matrix[2][3] = -cameraPos.dot(forward_vec);
+    return matrix;
+}
+
+namespace SIMD
+{
+
+struct Mat4ss
+{
+    __m128 mat[4];
+    Mat4ss()
+    {
+        /*    for (int i = 0; i < 4; ++i)
+                mat[i] = _mm_setzero_ps();*/
+    }
+
+    Mat4ss(Mat4f const &m)
+    {
+        mat[0] = _mm_set_ps(m.mat[0][0], m.mat[0][1], m.mat[0][2], m.mat[0][3]);
+        mat[1] = _mm_set_ps(m.mat[1][0], m.mat[1][1], m.mat[1][2], m.mat[1][3]);
+        mat[2] = _mm_set_ps(m.mat[2][0], m.mat[2][1], m.mat[2][2], m.mat[2][3]);
+        mat[3] = _mm_set_ps(m.mat[3][0], m.mat[3][1], m.mat[3][2], m.mat[3][3]);
+    }
+
+    Mat4ss(float s)
+    {
+        mat[0] = _mm_set_ps(s, 0.0f, 0.0f, 0.0f);
+        mat[1] = _mm_set_ps(0.0f, s, 0.0f, 0.0f);
+        mat[2] = _mm_set_ps(0.0f, 0.0f, s, 0.0f);
+        mat[3] = _mm_set_ps(0.0f, 0.0f, 0.0f, s);
+    }
+
+    Vec4ss mul_dp(Vec4ss const &vec) const
+    {
+
+        float ans[4];
+        for (int i = 0; i < 4; ++i)
+        {
+            ans[i] = _mm_cvtss_f32(_mm_dp_ps(mat[i], vec.vec, 0xF1));
+        }
+        return Vec4ss(ans[0], ans[1], ans[2], ans[3]);
+    }
+
+    Vec4ss mul_hadd(Vec4ss const &vec) const
+    {
+        auto a = _mm_mul_ps(this->mat[0], vec.vec);
+        auto b = _mm_mul_ps(this->mat[1], vec.vec);
+        auto c = _mm_mul_ps(this->mat[2], vec.vec);
+        auto d = _mm_mul_ps(this->mat[3], vec.vec);
+        return Vec4ss(_mm_hadd_ps(_mm_hadd_ps(d, c), _mm_hadd_ps(b, a)));
+    }
+
+    Vec4ss operator*(Vec4ss const &vec) const
+    {
+        // now write SIMD matrix multiplication here .. lmao
+        // Now remains matrix multiplication
+        // Around 5 times faster than usual mat4f * vec4f
+
+        // Older version
+        /*
+        float ans[4];
+        for (int i = 0; i < 4; ++i) {
+            ans[i] = _mm_cvtss_f32(_mm_dp_ps(mat[i], vec.vec, 0xF1));
+        }
+        return Vec4ss(ans[0], ans[1], ans[2], ans[3]);
+        */
+
+        // There's this rumor that dot product is actually slow on modern processors
+        // So lets try writing it in a different way
+        // This apparently is a little faster
+        auto a = _mm_mul_ps(this->mat[0], vec.vec);
+        auto b = _mm_mul_ps(this->mat[1], vec.vec);
+        auto c = _mm_mul_ps(this->mat[2], vec.vec);
+        auto d = _mm_mul_ps(this->mat[3], vec.vec);
+        return Vec4ss(_mm_hadd_ps(_mm_hadd_ps(d, c), _mm_hadd_ps(b, a)));
+    }
+
+    Vec4f operator*(Vec4f const &ivec) const
+    {
+        // now write SIMD matrix multiplication here .. lmao
+        // Now remains matrix multiplication
+        // Around 5 times faster than usual mat4f * vec4f
+
+        // Older version
+        /*
+        float ans[4];
+        for (int i = 0; i < 4; ++i) {
+            ans[i] = _mm_cvtss_f32(_mm_dp_ps(mat[i], vec.vec, 0xF1));
+        }
+        return Vec4ss(ans[0], ans[1], ans[2], ans[3]);
+        */
+
+        // There's this rumor that dot product is actually slow on modern processors
+        // So lets try writing it in a different way
+        // This apparently is a little faster
+        Vec4ss vec = Vec4ss(ivec);
+        auto a = _mm_mul_ps(this->mat[0], vec.vec);
+        auto b = _mm_mul_ps(this->mat[1], vec.vec);
+        auto c = _mm_mul_ps(this->mat[2], vec.vec);
+        auto d = _mm_mul_ps(this->mat[3], vec.vec);
+        vec = Vec4ss(_mm_hadd_ps(_mm_hadd_ps(d, c), _mm_hadd_ps(b, a)));
+        return Vec4f(vec);
+    }
+
+    // This around 3-4 times faster
+    // Optimized ones 7-8 times
+    Mat4ss matrixmul_dp(Mat4ss const &vec) const
+    {
+        Mat4ss result;
+        // Transposed using SIMD
+        // maybe start with a transpose, since that sounds good
+        Mat4ss transposed = vec.transpose();
+        // hmm... its quite simple compared to what i have thought
+
+        // let compiler unroll the loop
+        auto dot = [](auto x, auto y) { return _mm_cvtss_f32(_mm_dp_ps(x, y, 0xf1)); };
+        for (int i = 0; i < 4; ++i)
+        {
+            result.mat[i] = _mm_set_ps(dot(mat[i], transposed.mat[0]), dot(mat[i], transposed.mat[1]),
+                                       dot(mat[i], transposed.mat[2]), dot(mat[i], transposed.mat[3]));
+        }
+        return result;
+    }
+    Mat4ss matrixmul_ps(Mat4ss const &vec) const
+    {
+        Mat4ss result;
+        __m128 a, b, c, d, r0, r1, r2, r3;
+        float  elems[4][4];
+        _mm_store_ps(elems[0], mat[0]);
+        _mm_store_ps(elems[1], mat[1]);
+        _mm_store_ps(elems[2], mat[2]);
+        _mm_store_ps(elems[3], mat[3]);
+        for (int i = 0; i < 4; ++i)
+        {
+            a             = _mm_set_ps1(elems[i][3]);
+            b             = _mm_set_ps1(elems[i][2]);
+            c             = _mm_set_ps1(elems[i][1]);
+            d             = _mm_set_ps1(elems[i][0]);
+            r0            = _mm_mul_ps(a, vec.mat[0]);
+            r1            = _mm_mul_ps(b, vec.mat[1]);
+            r2            = _mm_mul_ps(c, vec.mat[2]);
+            r3            = _mm_mul_ps(d, vec.mat[3]);
+
+            result.mat[i] = _mm_add_ps(_mm_add_ps(r0, r1), _mm_add_ps(r2, r3));
+        }
+
+        // Lets extract all the required parameters first here
+        return result;
+    }
+
+    Mat4ss operator*(Mat4ss const &vec) const
+    {
+        // Core operation .. better write a good simd thinking nicely
+        Mat4ss result;
+        //// Transposed using SIMD
+        //// maybe start with a transpose, since that sounds good
+        // Mat4ss transposed = vec.transpose();
+        //// hmm... its quite simple compared to what i have thought
+
+        //// let compiler unroll the loop
+        // auto dot = [](auto x, auto y) { return _mm_cvtss_f32(_mm_dp_ps(x, y, 0xf1)); };
+        // for (int i = 0; i < 4; ++i) {
+        //	result.mat[i] = _mm_set_ps(
+        //		dot(mat[i], transposed.mat[0]),
+        //		dot(mat[i], transposed.mat[1]),
+        //		dot(mat[i], transposed.mat[2]),
+        //		dot(mat[i], transposed.mat[3])
+        //	);
+        // }
+        //  Hmm... really ? This simple?
+        //  Lets do some benchmarking now .. First normal check
+        //  TODO :: Write this without using transpose of another matrix
+        //  Use broadcast functions here
+        __m128 a, b, c, d, r0, r1, r2, r3;
+        float  elems[4][4];
+        _mm_store_ps(elems[0], mat[0]);
+        _mm_store_ps(elems[1], mat[1]);
+        _mm_store_ps(elems[2], mat[2]);
+        _mm_store_ps(elems[3], mat[3]);
+        for (int i = 0; i < 4; ++i)
+        {
+            a             = _mm_set_ps1(elems[i][3]);
+            b             = _mm_set_ps1(elems[i][2]);
+            c             = _mm_set_ps1(elems[i][1]);
+            d             = _mm_set_ps1(elems[i][0]);
+            r0            = _mm_mul_ps(a, vec.mat[0]);
+            r1            = _mm_mul_ps(b, vec.mat[1]);
+            r2            = _mm_mul_ps(c, vec.mat[2]);
+            r3            = _mm_mul_ps(d, vec.mat[3]);
+
+            result.mat[i] = _mm_add_ps(_mm_add_ps(r0, r1), _mm_add_ps(r2, r3));
+        }
+
+        // Lets extract all the required parameters first here
+        return result;
+    }
+
+    // void show()
+    //{
+    //     for (int i = 0; i < 4; ++i)
+    //     {
+    //         Vec4ss(mat[i]).show_hori();
+    //     }
+    // }
+
+    // float sum_first_row()
+    //{
+    //     return Vec4ss(mat[0]).vector_sum();
+    // }
+
+    Mat4ss transpose() const
+    {
+        Mat4ss mat;
+        // Nice .. this version is almost twice faster
+        // matrix vector multiplication was way faster with simd
+        // Lets try shuffling just first two rows
+        __m128 a   = _mm_unpackhi_ps(this->mat[1], this->mat[0]);
+        __m128 b   = _mm_unpackhi_ps(this->mat[3], this->mat[2]);
+        __m128 c   = _mm_unpacklo_ps(this->mat[1], this->mat[0]);
+        __m128 d   = _mm_unpacklo_ps(this->mat[3], this->mat[2]);
+
+        mat.mat[0] = _mm_movehl_ps(a, b);
+        mat.mat[1] = _mm_movelh_ps(b, a);
+
+        mat.mat[2] = _mm_movehl_ps(c, d);
+        mat.mat[3] = _mm_movelh_ps(d, c);
+        // _MM_TRANSPOSE4_PS(mat.mat[0], mat.mat[1], mat.mat[2], mat.mat[3]);
+        return mat;
+    }
+
+    Mat4ss translate(const Vec3f &vec) const
+    {
+        Mat4ss temp;
+        temp.mat[0] = _mm_set_ps(1.0f, 0.0f, 0.0f, vec.x);
+        temp.mat[1] = _mm_set_ps(1.0f, 0.0f, 0.0f, vec.y);
+        temp.mat[2] = _mm_set_ps(1.0f, 0.0f, 0.0f, vec.z);
+        temp.mat[3] = _mm_set_ps(1.0f, 0.0f, 0.0f, 1.0f);
+        return this->operator*(temp);
+    }
+
+    Mat4ss rotateY(float angle)
+    {
+        Mat4ss matrix;
+        auto   c = std::cos(angle), s = std::sin(angle);
+
+        // matrix[0][0]  = std::cos(angle);
+        // matrix[2][0]  = -std::sin(angle);
+        // matrix[0][2]  = -matrix[2][0];
+        // matrix[2][2]  = matrix[0][0];
+
+        matrix.mat[0] = _mm_set_ps(c, 0.0f, s, 0.0f);
+        matrix.mat[1] = _mm_set_ps(0.0f, 1.0f, 0.0f, 0.0f);
+        matrix.mat[2] = _mm_set_ps(-s, 0.0f, c, 0.0f);
+        matrix.mat[3] = _mm_set_ps(0.0f, 0.0f, 0.0f, 1.0f);
+        return this->operator*(matrix);
+    }
+};
+
+inline Mat4ss lookAtMatrix(const Vec3f &cameraPos, const Vec3f &target, const Vec3f &up)
+{
+    auto forward_vec = (cameraPos - target).unit();
+    auto right       = Vec3f::Cross(up, forward_vec).unit();
+    auto now_up      = Vec3f::Cross(forward_vec, right).unit();
+
+    // Mat4 matrix(1.0f);
+    // matrix[0][0] = right.x;
+    // matrix[0][1] = right.y;
+    // matrix[0][2] = right.z;
+    // matrix[0][3] = -cameraPos.dot(right);
+
+    // matrix[1][0] = now_up.x;
+    // matrix[1][1] = now_up.y;
+    // matrix[1][2] = now_up.z;
+
+    // matrix[1][3] = -cameraPos.dot(now_up);
+
+    // matrix[2][0] = forward_vec.x;
+    // matrix[2][1] = forward_vec.y;
+    // matrix[2][2] = forward_vec.z;
+    // matrix[2][3] = -cameraPos.dot(forward_vec);
+
+    Mat4ss matrix;
+    matrix.mat[0] = _mm_set_ps(right.x, right.y, right.z, -cameraPos.dot(right));
+    matrix.mat[1] = _mm_set_ps(now_up.x, now_up.y, now_up.z, -cameraPos.dot(now_up));
+    matrix.mat[2] = _mm_set_ps(forward_vec.x, forward_vec.y, forward_vec.z, -cameraPos.dot(forward_vec));
+    matrix.mat[3] = _mm_set_ps(0.0f, 0.0f, 0.0f, 1.0f);
+    return matrix;
+}
+
+inline Mat4ss Perspective(float aspect_ratio, float fovy = 1.0f / 3 * 3.141592, float near_plane = 0.1f,
+                          float far_plane = 10.0f)
+{
+    Mat4ss matrix;
+    float  tan_fovy = std::sin(fovy) / std::cos(fovy);
+    float  top      = near_plane * tan_fovy;
+    float  right    = aspect_ratio * top;
+
+    // matrix[0][0]   = near_plane / right;
+    // matrix[1][1]   = near_plane / top;
+    // matrix[3][2]   = -1;
+    // matrix[2][3]   = (-2 * far_plane * near_plane) / (far_plane - near_plane);
+    // matrix[2][2]   = -(far_plane + near_plane) / (far_plane - near_plane);
+    // matrix[3][3]   = 0;
+
+    matrix.mat[0] = _mm_set_ps(near_plane / right, 0.0f, 0.0f, 0.0f);
+    matrix.mat[1] = _mm_set_ps(0.0f, near_plane / top, 0.0f, 0.0f);
+    matrix.mat[2] = _mm_set_ps(0.0f, 0.0f, -(far_plane + near_plane) / (far_plane - near_plane),
+                               (-2 * far_plane * near_plane) / (far_plane - near_plane));
+    matrix.mat[3] = _mm_set_ps(0.0f, 0.0f, -1.0f, 0.0f);
+    return matrix;
+}
+} // namespace SIMD
--- src/maths/vec.hpp
+++ src/maths/vec.hpp
@@ -6,6 +6,10 @@
 #include <cstdint>
 #include <type_traits>
 
+// TODO :: Add feature test macros here
+#include <intrin.h>
+#include <xmmintrin.h>
+
 #include <iostream>
 
 using float32 = float;
@@ -15,6 +19,11 @@ template <typename T>
 concept cNumeric = std::is_integral_v<T> || std::is_floating_point_v<T>;
 
 template <cNumeric T> struct Vec3;
+
+namespace SIMD
+{
+struct Vec4ss;
+}
 
 template <cNumeric T> struct Vec2
 {
@@ -142,11 +151,16 @@ template <cNumeric T> struct Vec3
         return x * x + y * y + z * z;
     }
 
-    constexpr Vec3 unit() const 
+    constexpr T dot(const Vec3 &vec) const
     {
-        float mag = norm(); 
+        return x * vec.x + y * vec.y + z * vec.z;
+    }
+
+    constexpr Vec3 unit() const
+    {
+        float mag = norm();
         if (mag == 0)
-            return Vec3(); 
+            return Vec3();
         return Vec3(x / mag, y / mag, z / mag);
     }
     template <cNumeric U> constexpr auto operator*(U scalar) const -> Vec3<decltype(scalar * x)>
@@ -159,18 +173,72 @@ template <cNumeric T> struct Vec3
         return Vec3(x / scalar, y / scalar, z / scalar);
     }
 
-    constexpr static Vec3 Cross(Vec3 const& vec1, Vec3 const& vec2)
+    constexpr static Vec3 Cross(Vec3 const &vec1, Vec3 const &vec2)
     {
-        Vec3 res; 
-        res.x = vec1.y * vec2.z - vec1.z * vec2.y; 
-        res.y = vec1.z * vec2.x - vec1.x * vec2.z; 
-        res.z = vec1.x * vec2.y - vec1.y * vec2.x; 
+        Vec3 res;
+        res.x = vec1.y * vec2.z - vec1.z * vec2.y;
+        res.y = vec1.z * vec2.x - vec1.x * vec2.z;
+        res.z = vec1.x * vec2.y - vec1.y * vec2.x;
         return res;
     }
 
     constexpr static T Dot(Vec3 const &vec1, Vec3 const &vec2)
     {
         return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
+    }
+
+    constexpr auto project(Vec3 const &vec)
+    {
+        return Vec3::Dot(*this, vec.unit());
+    }
+
+    Vec3 reflect(const Vec3 &normal_) const
+    {
+        auto normal = normal_.unit();
+        return *this - 2.0f * this->dot(normal) * normal;
+    }
+
+    Vec3 refract(const Vec3 &normal, float RI) const
+    {
+        // RI is the ratio of refractive index of source medium to destination medium
+        auto n_i = this->dot(normal);
+
+        auto k   = 1.0f - RI * RI * (1.0f - n_i * n_i);
+        if (k < 0)
+            return Vec3();
+        return RI * *this - (RI * n_i + std::sqrt(k)) * normal;
+    }
+
+    Vec3 refractTIR(const Vec3 &normal, float RI) const // Takes account into total internal reflection
+    {
+        // RI is the refractive index of source medium to destination medium
+        auto n_i = this->dot(normal);
+
+        // Now how to detect total internal reflection ?
+        // Some normal flipping required here ..
+
+        if (n_i < 0.0f)
+        {
+            n_i = -n_i;
+            // It is correct normal
+            auto k = 1.0f - RI * RI * (1.0f - n_i * n_i);
+            if (k < 0)
+                return Vec3();
+            return RI * *this - (RI * n_i + std::sqrt(k)) * normal;
+        }
+        else
+        {
+            // We got outer normal... flip to make it inside normal
+            auto nNormal = normal * -1.0f;
+            // Now it is the case of light moving from denser medium to rarer medium
+            // Now flip the refractive index and use above formula on it
+            RI     = 1.0f / RI;
+            auto k = 1.0f - RI * RI * (1.0f - n_i * n_i);
+            if (k < 0)
+                return Vec3();
+            return RI * *this - (RI * n_i + std::sqrt(k)) * nNormal;
+            // This should do it
+        }
     }
 };
 
@@ -221,7 +289,15 @@ template <cNumeric T> std::ostream &operator<<(std::ostream &os, Vec2<T> vec)
 
 template <cNumeric T> struct Vec4
 {
-    T x, y, z, w; // w is the fourth component
+    union {
+
+        struct
+        {
+            T x, y, z, w; // w is the fourth component
+        };
+        T elem[4];
+    };
+
     Vec4() = default;
 
     explicit Vec4(T a) : x{a}, y{a}, z{a}, w{a}
@@ -233,9 +309,13 @@ template <cNumeric T> struct Vec4
     explicit Vec4(Vec3<T> xyz, T w) : x{xyz.x}, y{xyz.y}, z{xyz.z}, w{w}
     {
     }
+    explicit Vec4(Vec3<T> vec) : x{vec.x}, y{vec.y}, z{vec.z}, w{1.0f}
+    {
+    }
     Vec4(T x, T y, T z, T w) : x{x}, y{y}, w{w}, z{z}
     {
     }
+    Vec4(SIMD::Vec4ss const &vec);
     Vec4 operator+(const Vec4 &vec) const
     {
         return Vec4(x + vec.x, y + vec.y, z + vec.z, w + vec.w);
@@ -262,27 +342,18 @@ template <cNumeric T> struct Vec4
         return Vec4<decltype(scalar * x)>(x * scalar, y * scalar, z * scalar, w * scalar);
     }
 
-    Vec4 PerspectiveDivide() const 
+    T dot(Vec4 vec) const
     {
-        // Don't waste information 
+        return x * vec.x + y * vec.y + z * vec.z + w * vec.w;
+    }
+    Vec4 PerspectiveDivide() const
+    {
+        // Don't waste information
         return Vec4(x / w, y / w, z / w, 1.0f / w);
     }
     T &operator[](size_t index)
     {
-        // LOL ..
-        switch (index)
-        {
-        case 0:
-            return x;
-        case 1:
-            return y;
-        case 2:
-            return z;
-        case 3:
-            return w;
-        default:
-            assert("!Vec4 out of range");
-        }
+        return elem[index];
     }
     const T &operator[](size_t index) const
     {
@@ -311,9 +382,100 @@ template <cNumeric U, cNumeric T> auto operator*(U scalar, const Vec4<T> &vec) -
 using Vec4f  = Vec4<float>;
 using Vec4u8 = Vec4<uint8_t>;
 
-template <cNumeric T> 
-inline std::ostream &operator<<(std::ostream& os, const Vec4<T>& vec)
+template <cNumeric T> inline std::ostream &operator<<(std::ostream &os, const Vec4<T> &vec)
 {
     return os << "Vec4() -> "
               << "x : " << vec.x << ", y : " << vec.y << ", z : " << vec.z << ", w : " << vec.w << std::endl;
+}
+
+namespace SIMD
+{
+struct Vec4ss
+{
+    // These elements aren't mean to be accessed directly and should only be used for high performance operations
+    // So no union of __m128 and float[4] thingy
+    __m128 vec;
+    Vec4ss(float x, float y, float z, float w)
+    {
+        vec = _mm_set_ps(x, y, z, w);
+    }
+    Vec4ss(float x)
+    {
+        vec = _mm_set_ps1(x);
+    }
+    Vec4ss() = default;
+    Vec4ss(__m128 elem) : vec{elem}
+    {
+    }
+
+    Vec4ss(const Vec4f &vecf) : vec{_mm_set_ps(vecf.x, vecf.y, vecf.z, vecf.w)}
+    {
+    }
+    // Dot product of two vector float vectors
+    float dot(const Vec4ss &vec) const
+    {
+        __m128 res = _mm_dp_ps(this->vec, vec.vec, 0xE1);
+        return _mm_cvtss_f32(res);
+    }
+    Vec4ss operator+(const Vec4ss &vec) const
+    {
+        return Vec4ss(_mm_add_ps(this->vec, vec.vec));
+    }
+    Vec4ss operator-(const Vec4ss &vec) const
+    {
+        return Vec4ss(_mm_sub_ps(this->vec, vec.vec));
+    }
+    // Element wise multiplication of vector types
+    Vec4ss operator*(const Vec4ss &vec1) const
+    {
+        return Vec4ss(_mm_mul_ps(this->vec, vec1.vec));
+    }
+
+    Vec4ss operator*(const float &val) const
+    {
+        return Vec4ss(_mm_mul_ps(_mm_set_ps1(val), this->vec));
+    }
+
+    int compare_f3_greater_eq_than_zero() const 
+    {
+        return (_mm_movemask_ps(_mm_cmpge_ps(vec, _mm_setzero_ps())) & 0x0F);
+    }
+
+    int compare_f3_lesser_eq_than_zero() const 
+    {
+        return (_mm_movemask_ps(_mm_cmple_ps(vec, _mm_setzero_ps())) & 0x0F);
+    }
+
+    Vec4ss swizzle_for_barycentric() const
+    {
+        return Vec4ss(_mm_shuffle_ps(vec, vec, _MM_SHUFFLE(2, 1, 3, 0)));
+    }
+
+    static int generate_mask(Vec4ss const& a, Vec4ss const& b, Vec4ss const& c)
+    {
+        auto a_m = a.compare_f3_greater_eq_than_zero(); 
+        auto b_m = b.compare_f3_greater_eq_than_zero(); 
+        auto c_m = c.compare_f3_greater_eq_than_zero(); 
+        return a_m & b_m & c_m;
+    }
+
+    static int generate_neg_mask(Vec4ss const& a, Vec4ss const& b, Vec4ss const& c)
+    {
+        auto a_m = a.compare_f3_lesser_eq_than_zero();
+        auto b_m = b.compare_f3_lesser_eq_than_zero();
+        auto c_m = c.compare_f3_lesser_eq_than_zero();
+        return a_m & b_m & c_m;
+    }
+};
+} // namespace SIMD
+
+template <cNumeric T> 
+Vec4<T>::Vec4(SIMD::Vec4ss const& vec)
+{
+    float a[4]; 
+    _mm_store_ps(a, vec.vec);
+    x = a[3]; 
+    y = a[2]; 
+    z = a[1]; 
+    w = a[0];
 }
